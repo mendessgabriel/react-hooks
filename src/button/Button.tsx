b2a3a3d1f3dbe2727a8f5fbc81477af8ad4eced6
Gabriel Mendes <gabriel_mendes1@outlook.com>
2020-03-19 23:58:41 -0300

    Ajustes de cores

--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -3,18 +3,19 @@ import React from 'react';
 // import "../panel/Album.css";
 import "bootstrap/dist/css/bootstrap.css"
 import IAlbuns from '../album/IAlbuns';
+import "../button/Button.css";
 
 const Button = (albuns: IAlbuns) => {
     if (albuns.visible) {
         return (
             <div>
-                <button className="btn btn-dark">{albuns.name}</button>
+                <button className="btn btn-warning borderColor">{albuns.name}</button>
             </div>
         );
     } else {
         return (
             <div>
-                <button className="btn btn-dark">Button</button>
+                <button className="btn btn-warning borderColor">Button</button>
             </div>
             )
     }
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,26 +3,31 @@ import React from 'react';
 import "../header/Header.css";
 import "bootstrap/dist/css/bootstrap.css"
 
-import { MdPersonOutline } from "react-icons/md";
+import { MdPersonOutline, MdSearch } from "react-icons/md";
 import { IconContext } from "react-icons";
 
 const Header = () => {
     return (
-
-        <div className="header-align card navbar navbar-light bg-light paligned row">
+        <div className="bg-warning header-align card navbar paligned row">
             <div className="container">
                 <div className="col-sm-1">
                     <IconContext.Provider value={{ color: "btn-dark", size: "2em", className: "global-class-name" }}>
                         <div>
                             <MdPersonOutline className="iconstyle" />
-                        </div>
+                        </div> 
                     </IconContext.Provider>
                 </div>
                 <div className="col-sm-8">
-                    <nav className="col-sm navbar navbar-light bg-light">
-                        <form className="paligned form-inline">
-                            <input className="navstyle form-control mr-sm-2" type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
-                            <button className="btn btn-dark my-2 my-sm-0" type="submit">Procurar</button>
+                    <nav className="bg-warning  col-sm navbar">
+                        <form className="paligned form-inline"> 
+                            <input className="bg-dark colorWhite navstyle form-control mr-sm-2" type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
+                            <button className="btn btn-dark my-2 my-sm-0" type="submit">
+                                <IconContext.Provider value={{ color: "btn-dark", size: "1.2em", className: "global-class-name" }}>
+                                    <div>
+                                        Procurar    <MdSearch />
+                                    </div>
+                                </IconContext.Provider>
+                            </button> 
                         </form>
                     </nav>
                 </div>
