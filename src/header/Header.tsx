d4d21d148312b437f672c5b3907a407e58d097aa
Gabriel Mendes <gabriel_mendes1@outlook.com>
2020-03-19 23:34:52 -0300

    Icone de user adicionado

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -2,20 +2,31 @@ import React from 'react';
 
 import "../header/Header.css";
 import "bootstrap/dist/css/bootstrap.css"
-import IArtists from '../artists/IArtists';
-import Album from '../album/Album';
-import IAlbuns from '../album/IAlbuns';
-import Button from '../button/Button';
+
+import { MdPersonOutline } from "react-icons/md";
+import { IconContext } from "react-icons";
 
 const Header = () => {
     return (
-        <div className="header-align card navbar navbar-light bg-light paligned">
-            <nav className="navbar navbar-light bg-light">
-                <form className="paligned form-inline">
-                    <input className="navstyle form-control mr-sm-2" type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
-                    <button className="btn btn-dark my-2 my-sm-0" type="submit">Procurar</button>
-                </form>
-            </nav>
+
+        <div className="header-align card navbar navbar-light bg-light paligned row">
+            <div className="container">
+                <div className="col-sm-1">
+                    <IconContext.Provider value={{ color: "btn-dark", size: "2em", className: "global-class-name" }}>
+                        <div>
+                            <MdPersonOutline className="iconstyle" />
+                        </div>
+                    </IconContext.Provider>
+                </div>
+                <div className="col-sm-8">
+                    <nav className="col-sm navbar navbar-light bg-light">
+                        <form className="paligned form-inline">
+                            <input className="navstyle form-control mr-sm-2" type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
+                            <button className="btn btn-dark my-2 my-sm-0" type="submit">Procurar</button>
+                        </form>
+                    </nav>
+                </div>
+            </div>
         </div>
     );
 }
