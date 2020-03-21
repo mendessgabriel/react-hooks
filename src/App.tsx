bf8efc0d888ee6309807362d8e02f82fb2e21157
Gabriel Mendes Martins - GMR <gabriel.martins@iteris.local>
2020-03-20 21:16:47 -0300

    Inicio func Search

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,10 @@ import Menu from './menu/Menu';
 import IAppGeneral from './IAppGeneral';
 
 function App() {
-
+  const [instantGenre, setInstanceGenre] = useState<IGenre[]>([]);
+  const [instantSongs, setInstantSongs] = useState<ISongs[]>([]);
+  const [instantAlbuns, setInstantAlbuns] = useState<IAlbuns[]>([])
+  const [instantArtist, setInstantArtist] = useState<IArtists>({age:0, name:"",albums:instantAlbuns,genre:instantGenre});
   const genreRETobj: IGenre[] = [{ genre: "RAP" }];
   const genreFBCobj: IGenre[] = [{ genre: "RAP" }];
 
@@ -24,7 +27,7 @@ function App() {
   const [songsRetNMD] = useState<ISongs[]>(songsTreNMD);
 
   // const songsRET: ISongs[] = [{name: "Invicto",duration: "",song: "saaa",album: albunsRET }];
-  const AlbunsRET: IAlbuns[] = [{ name: "Numa Margem Distante", visible: true, songs: songsRetNMD }, { name: "VIVAZ", visible: true, songs: songsRetVVZ }, { name: "REVEL", visible: true, songs: songsRetNMD }, { name: "AUDAZ", visible: true, songs: songsRetNMD }];
+  const AlbunsRET: IAlbuns[] = [{ name: "Numa Margem Distante", visible: true, songs: songsRetNMD, release: "15/11" }, { name: "VIVAZ", visible: true, songs: songsRetVVZ, release: "15/11" }, { name: "REVEL", visible: true, songs: songsRetNMD, release: "15/11" }, { name: "AUDAZ", visible: true, songs: songsRetNMD, release: "15/11" }];
 
   const filipeRet: IArtists = {
     name: "Filipe Ret",
@@ -39,17 +42,29 @@ function App() {
   const [albunsRET, setAlbunsRET] = useState<IAlbuns[]>(AlbunsRET);
   const [ret, setRET] = useState<IArtists>(filipeRet);
 
-  const [albunsFBC, setAlbunsFBC] = useState<IAlbuns[]>([{ name: "S.C.A", visible: true, songs: songsRetNMD }, { name: "PADRIM", visible: true, songs: songsRetNMD }]);
+  const [albunsFBC, setAlbunsFBC] = useState<IAlbuns[]>([{ name: "S.C.A", visible: true, songs: songsRetNMD, release: "15/11" }, { name: "PADRIM", visible: true, songs: songsRetNMD, release: "15/11" }]);
   const [fbc, setFBC] = useState<IArtists>({ name: "FBC", age: 28, albums: albunsFBC, genre: genreFBC });
+
+  const [currentArtists, setCurrentArtists] = useState<IArtists[]>([ret, fbc]);
+  const [artistsToBeDisplayed, setArtistsToBeDisplayed] = useState<IArtists[]>([instantArtist]);
 
   const appStart: IAppGeneral = { isMenuVisible: true };
   const [appGeneral, setAppGeneral] = useState<IAppGeneral>(appStart);
 
   const renderMenu = () => { if (!appGeneral.isMenuVisible) { return (<div>{Menu()}</div>) } else { return (<div></div>) } }
 
+  const searchArtistSong = (parametro: String) => {
+    currentArtists.forEach(element => {
+      if (element.name.includes(parametro.toString())) {
+        setArtistsToBeDisplayed([element]); //Its not working
+      }
+      console.log(artistsToBeDisplayed);  
+    });
+  }
+
   return (
     <div className="appFont">
-      {Header(appGeneral, setAppGeneral)}
+      {Header(appGeneral, setAppGeneral, searchArtistSong)}
       <div className="row">
         <div className="col-sm-2">
           {renderMenu()}
@@ -63,6 +78,7 @@ function App() {
               <div className="col-sm">
                 {Panel(fbc, setFBC)}
               </div>
+              {searchArtistSong}
             </div>
           </div>
         </div>
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import "bootstrap/dist/css/bootstrap.css"
 import "../header/Header.css";
@@ -8,7 +8,9 @@ import { MdPersonOutline, MdSearch } from "react-icons/md";
 import { IconContext } from "react-icons";
 import IAppGeneral from '../IAppGeneral';
 
-const Header = (menuVisibility: IAppGeneral, setAppGeneral: (appGeneral: IAppGeneral) => void) => {
+const Header = (menuVisibility: IAppGeneral, setAppGeneral: (appGeneral: IAppGeneral) => void, searchArtistSong: (paramentro:String) => void) => {
+
+    const [inputValue, setInputValue] = useState<String>(""); 
 
     const clickAdmin = () => {
         if(menuVisibility.isMenuVisible){
@@ -16,6 +18,10 @@ const Header = (menuVisibility: IAppGeneral, setAppGeneral: (appGeneral: IAppGen
         }else{
             setAppGeneral({...menuVisibility, isMenuVisible: true})
         }
+    }
+
+    const updateInputValue = (updateWord: String) => {
+        setInputValue(updateWord);
     }
 
     return (
@@ -31,14 +37,14 @@ const Header = (menuVisibility: IAppGeneral, setAppGeneral: (appGeneral: IAppGen
                 <div className="col-sm-8">
                     <nav className="bg-warning  col-sm navbar">
                         <form className="paligned form-inline">
-                            <input className="bg-dark colorWhite navstyle form-control mr-sm-2" type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
-                            <button className="btn btn-dark my-2 my-sm-0" type="submit">
-                                <IconContext.Provider value={{ color: "btn-dark", size: "1.2em", className: "global-class-name" }}>
+                            <input className="bg-dark colorWhite navstyle form-control mr-sm-2" onChange={(event) => updateInputValue(event.target.value)} type="search" placeholder="Procure um artista ou uma musica..." aria-label="Search" />
+                            <a className="btn btn-dark my-2 my-sm-0">
+                                <IconContext.Provider value={{ color: "btn-dark", size: "1.2em" , className: "global-class-name" }}>
                                     <div>
-                                        Procurar    <MdSearch />
+                                        Procurar   <MdSearch type="button" onClick={() => searchArtistSong(inputValue)}  />
                                     </div>
                                 </IconContext.Provider>
-                            </button>
+                            </a>
                         </form>
                     </nav>
                 </div>
